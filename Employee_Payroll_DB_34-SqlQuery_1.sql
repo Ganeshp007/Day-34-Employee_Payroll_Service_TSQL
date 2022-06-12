e6d3d7e11aef63e6acbb38887b470a92bf84c835
Ganeshp007 <potdart40.gp@gmail.com>
2022-06-12 10:52:11 +0530

    Added:  UC2_CreateTable_IN_Employee_Payroll_DB

--- Employee_Payroll_DB_34-SqlQuery_1.sql
+++ Employee_Payroll_DB_34-SqlQuery_1.sql
@@ -1,2 +1,10 @@
 ﻿create database Employee_Payroll_DB_Day_34
 use Employee_Payroll_DB_Day_34
+
+create Table employee_payroll
+(
+  ID  int NOT NULL Primary key Identity,
+  EmployeeName varchar(25) Not Null,
+  EmpSalary money,
+  JoiningDate date
+)
